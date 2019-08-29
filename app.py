a2a5e6ab76e82f468a69b8462ff667337562b517
Administrator <admin@example.com>
2019-08-29 11:33:28 +0900

    delete dummy files

--- app.py
+++ app.py
@@ -16,7 +16,7 @@ def after_request(response):
 
 @app.route('/')
 def index():
-    return Response('ETRI Detection Test 2019.08.29 #1')
+    return Response('ETRI Detection Test 2019.08.29 #2')
 
 
 @app.route('/local')
