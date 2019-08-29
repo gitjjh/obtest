13e688b00fd0a16428ae9776f1c04d1a41e998fd
Administrator <admin@example.com>
2019-08-29 11:52:00 +0900

    app.py updated

--- app.py
+++ app.py
@@ -16,7 +16,7 @@ def after_request(response):
 
 @app.route('/')
 def index():
-    return Response('ETRI Detection Test 2019.08.29 #3')
+    return Response('ETRI Detection Test 2019.08.29 #4')
 
 
 @app.route('/local')
