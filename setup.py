8818f34782ef4e9a0c2bf16e9f099b1889b07e83
Administrator <admin@example.com>
2019-12-04 19:05:18 +0900

    gg

--- setup.py
+++ setup.py
@@ -7,8 +7,8 @@ from setuptools import setup
 REQUIRED_PACKAGES = ['Pillow>=1.0', 'Flask', 'tensorflow', 'six', 'matplotlib']
 
 setup(
- #   name='webrtc_object_detection',
-  #  version='0.1',
+    name='webrtc_object_detection',
+    version='0.1',
   #  install_requires=REQUIRED_PACKAGES,
   #  include_package_data=True,
   #  packages=[p for p in find_packages()],
