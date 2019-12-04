50b969b67590162b0d3d0f1212966d20ce714acb
Administrator <admin@example.com>
2019-12-04 17:31:36 +0900

    updated setup.py

--- setup.py
+++ setup.py
@@ -7,12 +7,12 @@ from setuptools import setup
 REQUIRED_PACKAGES = ['Pillow>=1.0', 'Flask', 'tensorflow', 'six', 'matplotlib']
 
 setup(
-    name='webrtc_object_detection',
-    version='0.1',
-    install_requires=REQUIRED_PACKAGES,
-    include_package_data=True,
-    packages=[p for p in find_packages()],
-    description='Tensorflow Object Detection with WebRTC',
+ #   name='webrtc_object_detection',
+  #  version='0.1',
+  #  install_requires=REQUIRED_PACKAGES,
+  #  include_package_data=True,
+  #  packages=[p for p in find_packages()],
+  #  description='Tensorflow Object Detection with WebRTC',
 )
 
 #'''Download the Object Dectection directory'''
