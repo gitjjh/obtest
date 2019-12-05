dbf484cbf62af2dec6884b1c7da309dd2247e3d9
Administrator <admin@example.com>
2019-12-05 14:16:09 +0900

    port number updated

--- docker_build.sh
+++ docker_build.sh
@@ -2,6 +2,6 @@
 
 sudo docker build --network host -t mec-app-detectiontest -f ./Dockerfile .
 sleep 1
-sudo docker tag mec-app-detectiontest mec-repo:5000/mec-app-detectiontest
+sudo docker tag mec-app-detectiontest mec-repo:31200/mec-app-detectiontest
 sleep 1
-sudo docker push mec-repo:5000/mec-app-detectiontest
+sudo docker push mec-repo:31200/mec-app-detectiontest
