3ab37fdca38f94183162231c06a8d8f3d16faca3
gitjjh <45617790+gitjjh@users.noreply.github.com>
2019-12-06 12:20:27 +0900

    Update docker_build.sh

--- docker_build.sh
+++ docker_build.sh
@@ -2,6 +2,6 @@
 
 sudo docker build --network host -t mec-app-detectiontest-copy -f ./Dockerfile .
 sleep 1
-sudo docker tag mec-app-detectiontest mec-repo:5000/mec-app-detectiontest-copy
+sudo docker tag mec-app-detectiontest-copy mec-repo:5000/mec-app-detectiontest-copy
 sleep 1
 sudo docker push mec-repo:5000/mec-app-detectiontest-copy
